3584ffd312d399a735338f0c9140bbe2b268459c
Beatriz Gomes <biaakiddo@gmail.com>
2022-05-03 10:05:09 -0300

    Deleted default migrations and created new ones

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -12,16 +12,7 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamps();
-        });
+    {  
     }
 
     /**
@@ -30,7 +21,6 @@ return new class extends Migration
      * @return void
      */
     public function down()
-    {
-        Schema::dropIfExists('personal_access_tokens');
+    { 
     }
 };
