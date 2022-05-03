7af23ebb068bf85bacbe10a94eca7cc92891ec0d
Beatriz Gomes <biaakiddo@gmail.com>
2022-05-03 14:31:21 -0300

    Created updateProduct routes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -29,4 +29,34 @@ class ProductController extends Controller
         $product = Product::find($id)->delete();
         return ["msg" => "Produto deletado!"];
     }
+
+    public static function updateProductName($id, Request $request){
+        $product = Product::find($id);
+
+        $product->name = $request->get("name");
+
+        $product->save();
+
+        return ["msg" => "Nome do produto foi atualizado!"];
+    }
+
+    public static function updateProductDescription($id, Request $request){
+        $product = Product::find($id);
+
+        $product->description = $request->get("description");
+
+        $product->save();
+
+        return ["msg" => "Descrição do produto foi atualizada!"];
+    }
+
+    public static function updateProductPrice($id, Request $request){
+        $product = Product::find($id);
+
+        $product->price = $request->get("price");
+
+        $product->save();
+
+        return ["msg" => "Preço do produto foi atualizado!"];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,10 @@ Route::get('/products', [ProductController::class, 'getProducts']);
 
 Route::get('/product/delete/{id}', [ProductController::class, 'deleteProduct']);
 
+Route::put('/product-name/{id}', [ProductController::class, 'updateProductName']);
+
+Route::put('/product-description/{id}', [ProductController::class, 'updateProductDescription']);
+
+Route::put('/product-price/{id}', [ProductController::class, 'updateProductPrice']);
+
+
